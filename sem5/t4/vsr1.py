0e5acb70dbb159a82d288347fe32e8b83a88411f
Elizaveta_Shekhovtcova <54999570+ShekhovtcovaE@users.noreply.github.com>
2021-06-28 18:27:18 +0300

    Create vsr1.py

---
+++ sem5/t4/vsr1.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+
+# Добавить загрузку файла
+
+data = pandas.read_csv("houses.csv")
+
+x = data['square_house']
+y = data['cost']
+
+x = x[~np.isnan(y)]
+y = y[~np.isnan(y)]
+
+def error(f, x, y):
+  return np.sum((f(x) - y)**2)
+    
+f1p, residuals, rank, sv, rcond = np.polyfit(x, y, 1, full=True)
+f1 = np.poly1d(f1p)
+fx = np.linspace(min(x),max(x),500) 
+toPredict = [1650.3, 2200.4]
+forecast = f1(toPredict)
+#print(forecast)
+print(forecast, error(f1, toPredict[0], toPredict[1]))
+
+plt.scatter(x, y, s=10)
+plt.plot(fx,f1(fx),linewidth=1.0,color='r')
+#plt.title('Трафик веб-сайта за последний месяц')
+#plt.xlabel("время")
+#plt.ylabel("запросы/час")
+#plt.xticks([w*7*24 for w in range(10)],["неделя %i" % w for w in range(10)])
+plt.autoscale(tight=True)
+plt.grid(True, linestyle="-", color='0.8')
+plt.show()
+
+f1p, residuals, rank, sv, rcond = np.polyfit(x, y, 2, full=True)
+f1 = np.poly1d(f1p)
+forecast = f1(toPredict)
+print(forecast, error(f1, toPredict[0], toPredict[1]))
+
+plt.scatter(x, y, s=10)
+plt.plot(fx,f1(fx),linewidth=1.0,color='r')
+plt.autoscale(tight=True)
+plt.grid(True, linestyle="-", color='0.8')
+plt.show()
+
+f1p, residuals, rank, sv, rcond = np.polyfit(x, y, 3, full=True)
+f1 = np.poly1d(f1p)
+forecast = f1(toPredict)
+print(forecast, error(f1, toPredict[0], toPredict[1]))
+
+plt.scatter(x, y, s=10)
+plt.plot(fx,f1(fx),linewidth=1.0,color='r')
+plt.autoscale(tight=True)
+plt.grid(True, linestyle="-", color='0.8')
+plt.show()
